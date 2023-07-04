fbc6ffb52e44963b24c778c6140d820fe028aa59
Felipe Jurado <felipejm9805@gmail.com>
2023-07-04 13:19:42 -0500

    feat(detection): add env variable

--- detection/main.py
+++ detection/main.py
@@ -6,23 +6,49 @@ import easyocr
 import util
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 
+@app.route('/', methods=['GET'])
+def root_endpoint():
+    return """
+    <html>
+    <head>
+        <title>Home Detection Page</title>
+    </head>
+    <body>
+        <h1>Welcome to the Home Detection Page!</h1>
+    </body>
+    </html>
+    """
+
+
+@app.route('/default', methods=['GET'])
+def default_endpoint():
+    return """
+    <html>
+    <head>
+        <title>Default Endpoint</title>
+    </head>
+    <body>
+        <h1>Welcome to the default endpoint!</h1>
+    </body>
+    </html>
+    """
+
+
 @app.route('/detection', methods=['GET'])
 def api_endpoint():
-
     # define constants
     model_cfg_path = os.path.join('.', 'model', 'cfg', 'darknet-yolov3.cfg')
     model_weights_path = os.path.join('.', 'model', 'weights', 'model.weights')
     class_names_path = os.path.join('.', 'model', 'class.names')
-
     input_dir = "./data"
-   
+
     for img_name in os.listdir(input_dir):
         img_path = os.path.join(input_dir, img_name)
-       
 
         # load class names
         with open(class_names_path, 'r') as f:
@@ -33,9 +59,7 @@ def api_endpoint():
         net = cv2.dnn.readNetFromDarknet(model_cfg_path, model_weights_path)
 
         # load image
-
         img = cv2.imread(img_path)
-
         H, W, _ = img.shape
 
         # convert image
@@ -43,18 +67,17 @@ def api_endpoint():
 
         # get detections
         net.setInput(blob)
-
         detections = util.get_outputs(net)
 
         # bboxes, class_ids, confidences
         bboxes = []
         class_ids = []
         scores = []
-        
+
         for detection in detections:
             # [x1, x2, x3, x4, x5, x6, ..., x85]
             bbox = detection[:4]
-            
+
             xc, yc, w, h = bbox
             bbox = [int(xc * W), int(yc * H), int(w * W), int(h * H)]
 
@@ -96,31 +119,18 @@ def api_endpoint():
                 license_plate_gray, 64, 255, cv2.THRESH_BINARY_INV)
 
             output = reader.readtext(license_plate_thresh)
-            
+
             for out in output:
                 text_bbox, text, text_score = out
                 # if text_score > 0.1:
                 output_text = text
-                print(text,  text_score)
+                print(text, text_score)
 
-        # plt.figure()
-        # plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-        # plt.figure()
-        # plt.imshow(cv2.cvtColor(license_plate, cv2.COLOR_BGR2RGB))
-
-        # plt.figure()
-        # plt.imshow(cv2.cvtColor(license_plate_gray, cv2.COLOR_BGR2RGB))
-
-        # plt.figure()
-        # plt.imshow(cv2.cvtColor(license_plate_thresh, cv2.COLOR_BGR2RGB))
-        # plt.show()
-
-    response = jsonify(message=text)
-
+    response = jsonify(message=output_text)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT_DETECTION', 5000))
+    app.run(port=port)
